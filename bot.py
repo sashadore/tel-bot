6f448f7555a6805e0547ed624f57f0189b08eed7
sashadore <alexandra.usacheva@yandex.com>
2025-05-23 14:57:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот помощник.")
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
