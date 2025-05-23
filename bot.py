59bba361dddf953c6a83fb953d14bdf29fb56bf6
sashadore <alexandra.usacheva@yandex.com>
2025-05-23 15:28:05 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот помощник.")
 
-
+print("Токен в переменной окружения:", TOKEN)
+ 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
